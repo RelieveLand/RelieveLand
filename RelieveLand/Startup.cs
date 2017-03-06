dafb8c56d0a5211555c14b7b4d46db78f98d10a0
Andy Humble <humbleandrew13@gmail.com>
2017-03-06 14:33:33 -0500

    initial commit

---
+++ RelieveLand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RelieveLand.Startup))]
+namespace RelieveLand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
