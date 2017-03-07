d7a4a242e3829c1cc5f9466dc9e2c6ce6715cfec
Andy Humble <humbleandrew13@gmail.com>
2017-03-07 14:56:06 -0500

    Built Establishment and Review Models

---
+++ RelieveLand/Models/EstablishmentModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RelieveLand.Models
+{
+    public class EstablishmentModels
+    {
+        [Key]
+        public int EstID { get; set; }
+
+        [DisplayName("Establishment Name")]
+        public string EstName { get; set; }
+        [DisplayName("Link to image of establishment or restroom")]
+        public string EstImage { get; set; }
+        [DisplayName("Establishment Address")]
+        public string EstAddress { get; set; }
+        [DisplayName("Zip Code")]
+        public int ZipCode { get; set; }
+        [DisplayName("Main Borough")]
+        public string BoroughPrimary { get; set; }
+        [DisplayName("Secondary Borough (if necessary)")]
+        public string BoroughSecondary { get; set; }
+        [DisplayName("Hours of Operation")]
+        public string HoursOfOper { get; set; }
+        [DisplayName("If Single stall only, check here")]
+        public bool SingleStall { get; set; }
+        [DisplayName("Hand Dryer Type")]
+        public string HandDryer { get; set; }
+        [DisplayName("Does it have a changing station?")]
+        public bool ChangingStation { get; set; }
+        [DisplayName("Must make a purchase to use restroom?")]
+        public bool PurchaseNeeded { get; set; }
+        [DisplayName("Does it have a handicap specific stall?")]
+        public bool HandicapStall { get; set; }
+        [DisplayName("Feminine hygiene products?")]
+        public bool HygieneProducts { get; set; }
+        [DisplayName("Family/Unisex Option?")]
+        public bool FamilyRestroom { get; set; }
+        [DisplayName("Any Extras?")]
+        public string Extras { get; set; }
+
+        [DisplayName("Overall Average")]
+        public float OverallAvg { get; set; }
+        [DisplayName("Odor Average")]
+        public float OdorAvg { get; set; }
+        [DisplayName("Appearance Average")]
+        public float AppearAvg { get; set; }
+
+        public virtual ICollection<EstablishmentModels> Establishments { get; set; }
+    }
+}
---
+++ RelieveLand/Models/ReviewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RelieveLand.Models
+{
+    public class ReviewModels
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [DisplayName("Review Time Submitted")]
+        public string ReviewTime { get; set; }
+        [DisplayName("Overall Restroom Rating")]
+        public int OverallRating { get; set; }
+        [DisplayName("Odor Rating")]
+        public int OdorRating { get; set; }
+        [DisplayName("Appearance Rating")]
+        public int AppearRating { get; set; }
+        [DisplayName("Additional comments")]
+        public string UserComments { get; set; }
+
+        [ForeignKey("EstablishmentModels")]
+        public int EstID { get; set; }
+        public virtual EstablishmentModels Establishments { get; set; }
+    }
+}
